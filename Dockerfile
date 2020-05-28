2d183f9cca6c8300409ce0ac5626554498cbbe2b
Soypete <34897832+Soypete@users.noreply.github.com>
2020-05-28 15:03:34 -0600

    running docker file

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,25 @@
-FROM		alpine
-RUN             apk update && apk add make gcc linux-headers git perl musl-dev go
+FROM	golang:1.13-alpine3.10
+RUN     apk update && apk add make gcc g++ linux-headers git perl musl-dev
 RUN		git clone https://github.com/xianyi/OpenBLAS && cd OpenBLAS && make && make PREFIX=/usr install
 RUN		mkdir -p /go/src /go/bin /go/pkg
 ENV		GOPATH=/go
-RUN		go get github.com/gonum/blas github.com/sjwhitworth/golearn
+# RUN     go version
+RUN go get -u github.com/gonum/blas
+RUN		go get -u  github.com/sjwhitworth/golearn github.com/kniren/gota/dataframe\ 
+         && go get -u -t gonum.org/v1/gonum/... gonum.org/v1/plot/...
+
+ENV foo src/github.com/Soypete/golearn
+RUN mkdir -p src/github.com/Soypete/golearn
+RUN ls
+RUN ls src
+RUN ls src/github.com
+WORKDIR ${foo}   # WORKDIR /bar
+RUN ls 
+
+COPY   . src/github.com/Soypete/golearn
+RUN     ls src/github.com/Soypete/golearn/examples/linear-regression/graphs
+# RUN     cd src/github.com/Soypete/golearn
+# RUN     pwd
+# RUN     go build ${foo}/examples/linear-regression/main.go
+# CMD     [.${foo}/examples/linear-regression/linear-regression]
+RUN     go run ${foo}/examples/linear-regression/main.go
--- examples/linear-regression/main.go
+++ examples/linear-regression/main.go
@@ -2,120 +2,76 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"os"
 
-	"github.com/kniren/gota/dataframe"
 	"github.com/sjwhitworth/golearn/base"
 	"github.com/sjwhitworth/golearn/evaluation"
 	regression "github.com/sjwhitworth/golearn/linear_models"
-	"gonum.org/v1/plot"
-	"gonum.org/v1/plot/plotter"
-	"gonum.org/v1/plot/vg"
 )
 
+const filepath = `src/github.com/Soypete/golearn/examples/linear-regression`
+
 func main() {
-	// Get data from csv file that contains advertising statics.
-	advertFile, err := os.Open("../datasets/Advertising.csv")
-	if err != nil {
-		log.Fatal(err)
-	}
 
-	advertDF := dataframe.ReadCSV(advertFile)
-	advertSummary := advertDF.Describe()
-	fmt.Println(advertSummary)
-	advertFile.Close()
-	// visualize data information
-	for _, colName := range advertDF.Names() {
-		plotVals := make(plotter.Values, advertDF.Nrow())
-		for i, floatVal := range advertDF.Col(colName).Float() {
-			plotVals[i] = floatVal
-		}
-		p, err := plot.New()
-		if err != nil {
-			log.Fatal(err)
-		}
-		p.Title.Text = fmt.Sprintf("Histogram of a %s", colName)
-		h, err := plotter.NewHist(plotVals, 16)
-		if err != nil {
-			log.Fatal(err)
-		}
-		h.Normalize(1)
-		p.Add(h)
-		if err := p.Save(4*vg.Inch, 4*vg.Inch, "graphs/"+colName+"_hist.png"); err != nil {
-			log.Fatal(err)
-		}
-	}
-	// we are comparing all of the other columns to the Sales column making Sales the dependent Variable
-	ySales := advertDF.Col("Sales").Float()
+	//TODO: add graph export
+	// // Get data from csv file that contains advertising statics.
+	// advertFile, err := os.Open(filepath + "/data/Advertising.csv")
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
 
-	for _, colName := range advertDF.Names() {
-		pts := make(plotter.XYs, advertDF.Nrow())
-		for i, floatVal := range advertDF.Col(colName).Float() {
-			pts[i].X = floatVal
-			pts[i].Y = ySales[i]
-		}
-		p, err := plot.New()
-		if err != nil {
-			log.Fatal(err)
-		}
-		p.X.Label.Text = colName
-		p.Y.Label.Text = "y"
-		p.Add(plotter.NewGrid())
-		s, err := plotter.NewScatter(pts)
-		if err != nil {
-			log.Fatal(err)
-		}
-		s.GlyphStyle.Radius = vg.Points(3)
-		p.Add(s)
-		if err := p.Save(4*vg.Inch, 4*vg.Inch, "graphs/"+colName+"_scatter.png"); err != nil {
-			log.Fatal(err)
-		}
-	}
-	// // manual train/test split
-	// trainingNum := (4 * advertDF.Nrow()) / 5
-	// testNum := advertDF.Nrow() / 5
-	// // adjust for odd amount
-	// if trainingNum+testNum < advertDF.Nrow() {
-	// 	trainingNum++
-	// }
-	// trainingIndex := make([]int, trainingNum)
-	// testIndex := make([]int, testNum)
-	// for i := 0; i < trainingNum; i++ {
-	// 	trainingIndex[i] = i
-	// }
-	// for i := 0; i < testNum; i++ {
-	// 	testIndex[i] = trainingNum + i
-	// }
-	// // randomize data
-	// trainingDF := advertDF.Subset(trainingIndex)
-	// testDF := advertDF.Subset(testIndex)
-	// setMap := map[int]dataframe.DataFrame{
-	// 	0: trainingDF,
-	// 	1: testDF,
-	// }
-	// for i, setName := range []string{"data/training.csv", "data/test.csv"} {
-	// 	f, err := os.Create(setName)
+	// advertDF := dataframe.ReadCSV(advertFile)
+	// advertSummary := advertDF.Describe()
+	// fmt.Println(advertSummary)
+	// advertFile.Close()
+	// // visualize data information
+	// for _, colName := range advertDF.Names() {
+	// 	plotVals := make(plotter.Values, advertDF.Nrow())
+	// 	for i, floatVal := range advertDF.Col(colName).Float() {
+	// 		plotVals[i] = floatVal
+	// 	}
+	// 	p, err := plot.New()
 	// 	if err != nil {
 	// 		log.Fatal(err)
 	// 	}
-	// 	w := bufio.NewWriter(f)
-	// 	if err := setMap[i].WriteCSV(w); err != nil {
+	// 	p.Title.Text = fmt.Sprintf("Histogram of a %s", colName)
+	// 	h, err := plotter.NewHist(plotVals, 16)
+	// 	if err != nil {
+	// 		log.Fatal(err)
+	// 	}
+	// 	h.Normalize(1)
+	// 	p.Add(h)
+	// 	if err := p.Save(4*vg.Inch, 4*vg.Inch, filepath+"graphs/"+colName+"_hist.png"); err != nil {
 	// 		log.Fatal(err)
 	// 	}
 	// }
-	// trainCSV, err := os.Open("data/training.csv")
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	// readerTrain := csv.NewReader(trainCSV)
-	// readerTrain.FieldsPerRecord = 4
-	// trainingData, err := readerTrain.ReadAll()
-	// if err != nil {
-	// 	log.Fatal(err)
+	// // we are comparing all of the other columns to the Sales column making Sales the dependent Variable
+	// ySales := advertDF.Col("Sales").Float()
+
+	// for _, colName := range advertDF.Names() {
+	// 	pts := make(plotter.XYs, advertDF.Nrow())
+	// 	for i, floatVal := range advertDF.Col(colName).Float() {
+	// 		pts[i].X = floatVal
+	// 		pts[i].Y = ySales[i]
+	// 	}
+	// 	p, err := plot.New()
+	// 	if err != nil {
+	// 		log.Fatal(err)
+	// 	}
+	// 	p.X.Label.Text = colName
+	// 	p.Y.Label.Text = "y"
+	// 	p.Add(plotter.NewGrid())
+	// 	s, err := plotter.NewScatter(pts)
+	// 	if err != nil {
+	// 		log.Fatal(err)
+	// 	}
+	// 	s.GlyphStyle.Radius = vg.Points(3)
+	// 	p.Add(s)
+	// 	if err := p.Save(4*vg.Inch, 4*vg.Inch, filepath+"graphs/"+colName+"_scatter.png"); err != nil {
+	// 		log.Fatal(err)
+	// 	}
 	// }
 
-	data, err := base.ParseCSVToInstances("../datasets/Advertising.csv", true)
+	data, err := base.ParseCSVToInstances(filepath+"/data/Advertising.csv", true)
 	if err != nil {
 		panic(err)
 	}
